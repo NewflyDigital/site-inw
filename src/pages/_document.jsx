0d0cef56d815dfa604917c55d81559eb4569a4a9
FAdrianoLima <fabioa.slima1@gmail.com>
2026-04-30 14:35:28 -0300

    Pixel e ajuste

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -26,8 +26,44 @@ class DocumentosSite extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
+
+          {/* Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}
+                (window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+
+                fbq('init', '1367351941822679');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          {/* End Meta Pixel Code */}
         </Head>
         <body>
+          {/* Meta Pixel (noscript) */}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1367351941822679&ev=PageView&noscript=1"
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
